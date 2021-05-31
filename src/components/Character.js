dc4fb269911e7b6e0acb07477b9649ecf2b7956f
Constance Nwaigwe <constancenwaigwe@gmail.com>
2021-05-31 02:52:39 -0400

    Create a CodeGrade submission

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,12 +1,50 @@
 // Write your Character component here
 
-import react, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
+//import axios from 'axios';
 
 
-const url = 'https://swapi.dev/api/people';
+//const url = 'https://swapi.dev/api/people';
 
-function myfunction(){
+
+const Characters = (props) => {
+    const {chars} = props;
+
+    return (
+        <div>
+            {chars.map((char) => {
+                return (
+                    <ul>
+                        <h3>{char.name}</h3>
+                        <li>Height: {char.height} </li>
+                        <li>Mass: {char.mass}</li>
+                        <li>Hair color: {char.hair_color}</li>
+                        <li>Skin color: {char.skin_color}</li>
+                        <li>Eye color: {char.eye_color}</li>
+                        <li>Birth year: {char.birth_year}</li>
+                        <li>Gender: {char.gender}</li>
+                    </ul>
+                );
+            })}
+        </div>
+    );
+};
+
+//export default Characters;
+
+function WithListLoading(Component) {
+    return function WihLoadingComponent({ isLoading, ...props }) {
+      if (!isLoading) return <Component {...props} />;
+      return (
+        <p style={{ textAlign: 'center', fontSize: '30px' }}>
+          Hold on, fetching data may take some time :)
+        </p>
+      );
+    };
+  }
+  export {Characters, WithListLoading};
+
+/*function myfunction(){
     axios.get(url)
         .then(res => {
             res.data.forEach( charitem => 
@@ -23,4 +61,4 @@ function myfunction(){
         .catch(err => {debugger})
 }
 
-export default myfunction;
+export default myfunction;*/
