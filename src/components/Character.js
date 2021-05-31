9519d1366c08bd13893565cb69b28e2f2a9d9dbe
Constance Nwaigwe <constancenwaigwe@gmail.com>
2021-05-31 00:27:02 -0400

    Create a CodeGrade submission

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+
+import react, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
+const url = 'https://swapi.dev/api/people';
+
+function myfunction(){
+    axios.get(url)
+        .then(res => {
+            res.data.forEach( charitem => 
+                {<div>
+                    <button onclick="myFunction()" className="dropbtn">Dropdown</button>
+                    <div id="myDropdown" className="dropdown-content">
+                        <p>Name: {charitem.name}</p>
+                        <p>Gender: {charitem.gender}</p>
+                        <p>Height: {charitem.height}</p>
+                    </div>
+                </div>}
+            )
+        })
+        .catch(err => {debugger})
+}
+
+export default myfunction;
